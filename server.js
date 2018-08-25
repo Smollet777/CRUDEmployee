04e2d6c3aac7b6fec3de3621c0ebe04d10fb9caf
Ivan Smollet <smollet777@bigmir.net>
2018-08-25 11:36:42 +0300

    employee model & controller

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 const {
   mongoose
 } = require('./db.js');
+var employeeController = require('./controllers/employeeController.js');
 
 var app = express();
 app.use(bodyParser.json());
 
 app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+
+
+app.use('/', employeeController);
