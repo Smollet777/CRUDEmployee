771996bd1d3835ee29bf1767ee49405d9391f925
Ivan Smollet <smollet777@bigmir.net>
2018-08-29 13:12:30 +0300

    show all employees

--- client/src/app/employee/employee.component.ts
+++ client/src/app/employee/employee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model';
 
 declare var M: any;
 
@@ -16,6 +17,8 @@ export class EmployeeComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.resetForm();
+    this.refreshEmployeeList();
   }
 
   resetForm(form?: NgForm) {
@@ -33,6 +36,12 @@ export class EmployeeComponent implements OnInit {
     this.employeeService.postEmployee(form.value).subscribe((res) => {
       this.resetForm(form);
       M.toast({ html: 'Saved successfully', classes: 'rounded' });
+    });
+  }
+
+  refreshEmployeeList() {
+    this.employeeService.getEmployeeList().subscribe((res) => {
+      this.employeeService.employees = res as Employee[];
     });
   }
 
