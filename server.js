b0911f39b7ec59e2791332717b57a71c48b4aeb5
Ivan Smollet <smollet777@bigmir.net>
2018-08-29 12:33:00 +0300

    cors

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const {
   mongoose
 } = require('./db.js');
-var employeeController = require('./controllers/employeeController.js');
+let employeeController = require('./controllers/employeeController.js');
 
-var app = express();
+let app = express();
 app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 
 app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
 
