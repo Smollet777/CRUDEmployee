6d3c3f80dc9178cf5d839cba3159f3030e9b7ba8
Ivan Smollet <smollet777@bigmir.net>
2018-08-30 13:28:29 +0300

    update & delete employee

--- client/src/app/employee/employee.component.ts
+++ client/src/app/employee/employee.component.ts
@@ -30,19 +30,43 @@ export class EmployeeComponent implements OnInit {
       office: '',
       salary: null
     };
+    this.refreshEmployeeList();
   }
 
   onSubmit(form: NgForm) {
-    this.employeeService.postEmployee(form.value).subscribe((res) => {
-      this.resetForm(form);
-      M.toast({ html: 'Saved successfully', classes: 'rounded' });
-    });
+    if (form.value._id === '') {
+      this.employeeService.postEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({ html: 'Saved successfully', classes: 'rounded' });
+      });
+    } else {
+      this.employeeService.putEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({ html: 'Updated successfully', classes: 'rounded' });
+      });
+    }
   }
 
   refreshEmployeeList() {
     this.employeeService.getEmployeeList().subscribe((res) => {
       this.employeeService.employees = res as Employee[];
     });
+  }
+
+  onEdit(emp: Employee) {
+    this.employeeService.selectedEmployee = emp;
+  }
+
+  onDelete(_id: string, form: NgForm) {
+    if (confirm('Are you sure to delete this record ?') === true) {
+      this.employeeService.deleteEmployee(_id).subscribe((res) => {
+        this.refreshEmployeeList();
+        this.resetForm(form);
+        M.toast({ html: 'Deleted successfully', classes: 'rounded' });
+      });
+    }
   }
 
 }
